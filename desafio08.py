98b499d0750479d480df19f640263dc80ee1f662
joaosopran <159202685+joaosopran@users.noreply.github.com>
2024-04-23 10:32:05 -0300

    desafio conversão de medidas

---
+++ desafio08.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um valor em metro: '))
+
+cent = (n * 10) * 10
+mili = ((n * 10) * 10) * 10
+
+print('O valor em centimetros é: ',cent,'cm', ' E o valor em milimetros é: ',mili,'mm')
