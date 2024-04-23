2b24149863c49fc36c379d529674a43c5452705e
joaosopran <159202685+joaosopran@users.noreply.github.com>
2024-04-23 12:26:33 -0300

    desafio 11 calculo de area e quantidade de tinta necessária

---
+++ desafio11.py
@@ -0,0 +1,8 @@
+larg = float(input('Qual a largura da parede: '))
+altu = float(input('Qual a altura da parede: '))
+
+area = larg * altu
+tinta = area / 2
+
+print('A area da parede é de: ',area,'m²')
+print('Para pintar essa parede, você precisará de: ',tinta,'L de tinta')
