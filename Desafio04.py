128f640e639526978cd18c1688d8b13dce7c937b
joaosopran <159202685+joaosopran@users.noreply.github.com>
2024-04-23 09:35:15 -0300

    Trabalhando com tipos de variaveis

---
+++ Desafio04.py
@@ -0,0 +1,9 @@
+a = input('Digite algo: ')
+
+print('o tipo desse valor é ', type(a))
+print('Só tem espaços ', a.isspace())
+print('é um numero: ', a.isnumeric())
+print('é alfabético ', a.isalpha())
+print('é alfanumérico', a.isalnum())
+print('está em maiusculas', a.isupper())
+print('está em minusculas', a.islower())
