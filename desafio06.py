f4bbc6477a57eb2b02ccee5abd72cd776ec40ef7
joaosopran <159202685+joaosopran@users.noreply.github.com>
2024-04-23 10:16:57 -0300

    Desafio com dobro, triplo e raiz quadrada

---
+++ desafio06.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um valor: '))
+dob = n * 2
+tri = n * 3
+raiz = n ** (1/2)
+
+print('O valor digitado foi: ', n,' O dobro do numero é : ',dob,' O triplo do número é: ',tri, ' a raiz é: ',raiz)
