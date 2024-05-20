d5614b515b6f2d48c6406eaf642d86a15a85e6d7
joaosopran <159202685+joaosopran@users.noreply.github.com>
2024-05-20 10:08:53 -0300

    desafio21 - reproduzir audio MP3

---
+++ desafio21.py
@@ -0,0 +1,15 @@
+import pygame
+import time
+
+pygame.init()
+
+pygame.mixer.music.load('ex021.mp3')
+
+pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy():
+    time.sleep(1)
+
+
+
+pygame.event.wait()
